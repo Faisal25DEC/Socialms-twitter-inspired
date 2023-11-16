a6ac02f8ecd9d3eef9e19dd871f6e98f10041024
Faisal25DEC <faisalmd25121999@gmail.com>
2023-11-16 22:58:11 +0530

    UI updates

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,15 +8,17 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   const router = useRouter();
+  console.log(router.pathname);
+
+  const colSpan =
+    router.pathname === "/messages" ? "lg:col-span-3" : "lg:col-span-2";
   return (
     <div className="h-screen bg-black">
       <div className="container h-full mx-auto xl:px-30 max-w:6xl">
         <div className="grid grid-cols-4 h-full">
           <Sidebar />
           <div
-            className={`col-span-3 lg:${
-              router.pathname === "/messages" ? "col-span-3" : "col-span-2"
-            } border-x-[1px] border-neutral-800`}
+            className={`col-span-3 ${colSpan} border-x-[1px] border-neutral-800`}
           >
             {children}
           </div>
